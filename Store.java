0052a3a1b2728658d0ed3548abf83886dc7e7bda
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-07-18 08:05:33 +0530

    constructor example

---
+++ Store.java
@@ -0,0 +1,31 @@
+public class Store
+{
+  static String storeName = "Tamil";
+  String prodName;
+  int price;
+
+public Store(String s,int x)                       //constructor - default,argument
+{                                                  //no return type,same name as class name
+  prodName = s;                                    //called automatically when object is created -default(JVM)
+  price = x;                                       //used for initializing object specific values(non static)
+}                                                  //if argument is passed - argument constructor
+
+public static void main(String[] args)
+{
+
+  System.out.println(Store.storeName);           //static variable is printed using class name,object name
+   
+ Store prod1 = new Store("abc",100);             //object 1
+ System.out.println(prod1.prodName);          
+ System.out.println(prod1.price);
+
+ Store prod2 = new Store("xyz",1000);            //object 2
+ System.out.println(prod2.prodName);
+ System.out.println(prod2.price);
+
+ 
+}
+
+
+
+}
