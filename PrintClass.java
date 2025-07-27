f3daededa8359a4a9c682d81a2243f201523c436
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-07-27 09:05:02 +0530

    types of inheritance

---
+++ PrintClass.java
@@ -0,0 +1,29 @@
+public class PrintClass
+{
+public static void main(String[] args)
+{
+  PrintClass pc = new PrintClass();            
+  pc.display();                     // method overloading
+  pc.display("abc"); 
+  pc.display(10);
+
+  System.out.println(10);          // println is default method 
+  System.out.println();            // method overloading
+  System.out.println("abc");
+
+}
+
+public void display(String s)
+{
+
+}
+public void display()
+{
+
+}
+
+public void display(int i)
+{
+
+}
+}
