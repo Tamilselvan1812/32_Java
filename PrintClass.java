0325710355ce58cde05259e070d91df38f1d3e73
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-07-28 08:03:10 +0530

    Println method

--- PrintClass.java
+++ PrintClass.java
@@ -3,12 +3,12 @@ public class PrintClass
 public static void main(String[] args)
 {
   PrintClass pc = new PrintClass();            
-  pc.display();                     // method overloading
+  pc.display();                      //method overloading
   pc.display("abc"); 
   pc.display(10);
 
-  System.out.println(10);          // println is default method 
-  System.out.println();            // method overloading
+  System.out.println(10);           //println is default method 
+  System.out.println();             //method overloading
   System.out.println("abc");
 
 }
