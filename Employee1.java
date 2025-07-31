2795876604c74b78e7a6b9dc9f6a579b15857222
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-07-31 08:13:52 +0530

    Interface

---
+++ Employee1.java
@@ -0,0 +1,27 @@
+public class Employee1 implements OfficeRules         // the interface must be "implemented" by another class with the implements keyword
+{
+
+public void comeOnTime()
+{
+  System.out.println("10am");                      // On implementation of an interface, you must override all of its methods
+}
+
+public void getSalary()
+{
+
+}
+
+public void takeLeave()
+{
+
+}
+
+public static void main(String[] args)
+{
+  
+  Employee1 em = new Employee1();
+  em.comeOnTime();
+
+}
+
+}
---
+++ OfficeRules.java
@@ -0,0 +1,9 @@
+public interface OfficeRules     // interface is not class
+{
+
+public void comeOnTime();        // interface methods do not have a body
+public void getSalary();         // interface methods are by default abstract
+public void takeLeave();         // an interface cannot contain a constructor (as it cannot be used to create objects)
+                                  
+
+}
