168bd691e4112e3dc16e13f7e0efed427dd1c054
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-08-02 07:40:50 +0530

    Interface

--- OfficeRules.java
+++ OfficeRules.java
@@ -3,6 +3,6 @@ public interface OfficeRules     // interface is not class
 
  public void comeOnTime();        // interface methods do not have a body
  public void getSalary();         // interface methods are by default abstract
- spublic void takeLeave();         // an interface cannot contain a constructor (as it cannot be used to create objects)
+ public void takeLeave();         // an interface cannot contain a constructor (as it cannot be used to create objects)
                                   
 }
