2c46244544d9abc34ea63514113c6028b898283a
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-08-04 08:21:55 +0530

    Abstraction

--- Son.java
+++ Son.java
@@ -4,13 +4,13 @@ public class Son extends Mother
 public static void main(String[] args)
 {
 
-  Son son = new Son();                 //so we create object in child class
+  Son son = new Son();                 // so we create object in child class
   son.study();
   son.workHard();
   son.motivate();
 }
 
-public void study()                  //The body is provided by the subclass
+public void study()                  // The body is provided by the subclass
 {
   System.out.println("BE");             
 }
