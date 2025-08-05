5c38d189d175aa8e719048174b11e86ffe5eb40b
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-08-05 08:10:03 +0530

    Encapsulation

---
+++ Rider.java
@@ -0,0 +1,17 @@
+public class Rider extends PetrolBunk
+
+{
+
+public static void main(String[] args)
+{
+ 
+PetrolBunk pb = new PetrolBunk();
+pb.price = 11;
+System.out.println(pb.price);
+
+
+
+}
+
+
+}
