c96ca77d32d8b7829e94c155ab4c220a4e5f172c
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-08-07 07:51:48 +0530

    Scanner and unchecked Exception

---
+++ MyScanner.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;                                              // we have to import first
+
+public class MyScanner
+ {
+    public static void main(String[] args)
+
+ {
+        Scanner scanner = new Scanner(System.in);                     // scanner is utility class that is used to read input
+                                                                      // create an object of the class and use any of the available methods found in the Scanner class documentation.
+        System.out.print("Enter no1: ");                       
+        int no1 = scanner.nextInt();
+
+        System.out.print("Enter no2: ");
+        int no2 = scanner.nextInt();
+
+   try
+        {
+            int result = no1 / no2;
+            System.out.println("Result of no1 / no2: " + result);
+        }
+   catch (ArithmeticException e)
+        {
+            System.out.println("Error: no2 should not be zero.");    // arithematic exception - unchecked exception (handled by try and catch)
+        }
+
+        scanner.close();
+    }
+}
