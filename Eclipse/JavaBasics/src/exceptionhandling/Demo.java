05fcffb28aaeb6e334aefbe93c3fba77d3d5b7bd
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-08-12 08:18:43 +0530

    Exception

---
+++ Eclipse/JavaBasics/src/exceptionhandling/Demo.java
@@ -0,0 +1,22 @@
+package exceptionhandling;
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		   
+		   
+		 int n1=10;
+		 int n2=0;
+		 
+		 try {
+		      int n =  n1/n2;
+		 }
+		 catch(Exception e)
+		 {
+			System.out.println("hi"); 
+		 }
+
+	}
+
+}
