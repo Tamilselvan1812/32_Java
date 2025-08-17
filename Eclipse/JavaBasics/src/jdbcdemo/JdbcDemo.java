3530ca3a717590324dc4e3b2201e887e9c29b7c2
Tamilselvan1812 <tamilselvankrishnasamy18@gmail.com>
2025-08-17 08:11:32 +0530

    JDBC

--- Eclipse/JavaBasics/src/jdbcdemo/JdbcDemo.java
+++ Eclipse/JavaBasics/src/jdbcdemo/JdbcDemo.java
@@ -1,14 +1,34 @@
 package jdbcdemo;
-import java.sql.*;
+import java.sql.*;                                                                             //1.import the package
 
 public class JdbcDemo {
-          public static void main(String[] args) throws ClassNotFoundException
+          public static void main(String[] args) throws Exception 
           {
         	  
         	 
-        		  Class.forName("com.mysql.cj.jdbc.Driver");
-        		  System.out.println("MySQL Driver loaded successfully");
-
+        		  
+//			 Class.forName("com.mysql.cj.jdbc.Driver");                                         //2.load and register the driver
+		     
+        	  String url = "jdbc:mysql://localhost:3306/jdbcforyoutube";
+        	  String username="root";
+        	  String password = "Kts@1812";
+        	  Connection con = DriverManager.getConnection(url,username,password);               //3.establish a connection 
+              Statement st = con.createStatement();                                              //4.create the statement(statement,prepared statement, callablestatement)
+              
+              String sql = "select * from student ";      
+              
+              ResultSet rs = st.executeQuery(sql);                                              //5.execute the statement
+              
+              while( rs.next())                                  // skipping the header
+              {
+            	  System.out.println(rs.getInt("rno")+" "+rs.getString("name")+" "+rs.getString("location")); //6.process the result
+              }                       
+              
+             
+              
+              con.close();                                                                                   //7.close the connection
+              
+             
 			 
           }
 }
